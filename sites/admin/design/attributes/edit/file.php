d74bfcb12838f53d95f3c3f3cd14ca9fd78d779f
Jean de GROMARD / Télétravail <jean.de.gromard@gmail.com>
2023-10-27 23:51:07 +0200

    cleanup, image attribute heritate from file attribute, possibility of big files from server, administration mechanismes improved

--- sites/admin/design/attributes/edit/file.php
+++ sites/admin/design/attributes/edit/file.php
@@ -1,30 +1,42 @@
-<?php if( $srcFile ): ?>
-    <div class="current-file-display">
-        <h2>Current file</h2>
+<div class="current-file-display" <?=$srcFile? '': 'style="display: none;"' ?>>
+    <?php if( $srcFile ): ?>
+        <h2 class="current-file-target">Current file</h2>
         
-        <a href="<?=$srcFile?>" target="_blank">
+        <a class="current-file-target" 
+           href="<?=$srcFile?>" 
+           target="_blank">
             <?=$this->values['file']?>
         </a>
-        
-        <a class="deleteFile" value="<?=$this->name.'@'.$this->type.'#filedelete'?>" >
-            <i class="fa fa-times"></i>
-        </a>
-    </div>
-<?php endif; ?>
-   
+    <?php endif; ?>
+    
+    <span class="new-file-target"></span>
+    <a class="delete-file">
+        <i class="fa fa-times"></i>
+    </a>
+</div>
 
-<p>
-    <h2>Browse file</h2>
-    
-    <input  type="file" 
-            class="changeFile"
-            name="<?=$this->name.'@'.$this->type.'#fileupload'?>" />
-    
-    <input  type="hidden" 
-            name="<?=$this->name.'@'.$this->type.'#file'?>" 
-            id="<?=$this->name.'@'.$this->type.'#file'?>" 
-            value="<?=$this->values['file']?>" />
-</p>
+<div class="change-file-container" <?=$srcFile? 'style="display: none;"': '' ?>>
+    <div>
+        <h2>Upload file</h2>
+
+        <input  type="file" 
+                class="change-file"
+                name="<?=$this->name.'@'.$this->type.'#fileupload'?>" />
+    </div>
+    <div>    
+        <h2>Move server file</h2>
+        <input  type="text" 
+                class="change-file"
+                name="<?=$this->name.'@'.$this->type.'#filemove'?>" />
+        <em>enter here full path filename</em>
+    </div>
+</div>
+
+<input  type="hidden" 
+        class="file-input"
+        name="<?=$this->name.'@'.$this->type.'#file'?>" 
+        id="<?=$this->name.'@'.$this->type.'#file'?>" 
+        value="<?=$this->values['file']?>" />
 
 <p>
     <h2>Filename</h2>
