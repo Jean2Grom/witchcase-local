ac8bd79eac83c7ac27620463e309125293ea89c6
Jean de GROMARD / Télétravail <jean.de.gromard@gmail.com>
2023-10-30 17:45:48 +0100

    add wysiwyg MIT licenced Trumbowyg editor

--- sites/admin/design/attributes/edit/text.php
+++ sites/admin/design/attributes/edit/text.php
@@ -1,3 +1,14 @@
-<textarea  id="<?=$this->type.'__'.$this->name?>" 
+<?php
+    $this->wc->website->context->addCssFile('../trumbowyg/dist/ui/trumbowyg.min.css');
+    $this->wc->website->context->addJsLibFile('../trumbowyg/dist/trumbowyg.min.js');
+    
+    $id = $this->type.'__'.$this->name;
+?>
+
+
+<textarea  id="<?=$id ?>" 
            name="<?=$this->tableColumns['value']?>"><?=$this->values['value']?></textarea>
 
+<script>
+    $('#<?=$id ?>').trumbowyg();
+</script>
