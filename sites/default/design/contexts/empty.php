5983adda1896b5a24bb4431680279f1af9c47f8d
Jean de GROMARD / Télétravail <jean.de.gromard@gmail.com>
2023-11-24 19:24:26 +0100

    first commit

--- sites/default/design/contexts/empty.php
+++ sites/default/design/contexts/empty.php
@@ -1,3 +1,13 @@
 <?php /** @var WC\Context $this */ ?>
 
+<?php foreach( $this->getJsLibFiles() as $jsLibFile ): ?>
+    <script src="<?=$jsLibFile?>"></script>
+<?php endforeach; ?>
+
+<?php foreach( $this->getCssFiles() as $cssFile ): ?>
+    <link   rel="stylesheet" 
+            type="text/css" 
+            href="<?=$cssFile?>" />
+<?php endforeach; ?>
+
 <?=$this->wc->witch()->result() ?>
