cf05a1bd0ac3b1c0cfbeeaf58410f3e3a3f3dae3
jean2grom <jean.de.gromard@gmail.com>
2024-06-19 17:33:18 +0200

    structure restriction interface -in progress

--- sites/admin/design/modules/structure/edit.php
+++ sites/admin/design/modules/structure/edit.php
@@ -21,54 +21,16 @@ $this->addJsFile('triggers.js');
     <em id="file-display"><?=$structure->file ?? ''?></em>
     <input id="file-input" type="text" name="file" value="<?=$structure->file ?>" />
 
-    <ul  class="global-data">
-        <li>
-            <div>Accepted contents</div>
-            <select id="add-accepted">
-                <option value="0">Select content type</option>
-                <?php foreach( $possibleTypes as $possibleType => $label ): ?>
-                    <option value="<?=$possibleType?>">
-                        <?=$label?>
-                    </option>
-                <?php endforeach; ?>                
-            </select>
-        </li>
-        <li>
-            <ul>
-                <?php foreach( $structure->require['accept'] ?? [] as $acceptedItem ): ?>
-                    <?=$acceptedItem?>
-                    <a><i class="fa fa-times"></i></a>
-                <?php endforeach; ?> 
-            </ul>
-        </li>
-        <li>
-            <div>Refused contents</div>
-            <select id="add-refused">
-                <option value="0">Select content type</option>
-                <?php foreach( $possibleTypes as $possibleType => $label ): ?>
-                    <option value="<?=$possibleType?>">
-                        <?=$label?>
-                    </option>
-                <?php endforeach; ?>                
-            </select>
-        </li>
-        <li>
-            <ul>
-                <?php foreach( $structure->require['refuse'] ?? [] as $refusedItem ): ?>
-                    <?=$refusedItem?>
-                    <a><i class="fa fa-times"></i></a>
-                <?php endforeach; ?> 
-            </ul>
-        </li>
-        <li>
-            <div>Minimum required</div>
-            <div><input type="number" min="0" value="<?=$structure->require['min'] ?? 0?>" /></div>
-        </li>
-        <li>
-            <div>Maximum allowed</div>
-            <div><input type="number" min="0" value="<?=$structure->require['max'] ?? 0?>" /></div>
-        </li>
-    </ul>
+    <div class="fieldsets-container">
+        <fieldset>
+            <legend>global restrictions</legend>
+            <?php 
+                $require    = $structure->require;
+                $this->wc->debug($require);
+                $name       = "global";
+                include $this->getIncludeDesignFile('edit/structure-require.php'); ?>
+        </fieldset>
+    </div>
 
     <div class="fieldsets-container">
         <?php foreach( $structure->structure?->composition ?? $structure->composition ?? [] as $item ): ?>
@@ -82,13 +44,16 @@ $this->addJsFile('triggers.js');
                 <ul>
                     <li>
                         <div>Name</div>
-                        <input type="text" name="content-name[]" value="<?=$item['name']?>" />
+                        <input type="text" name="<?=$item['name']?>-name[]" value="<?=$item['name']?>" />
                     </li>
                     <li>
                         <div>Type</div>
-                        <select name="content-type[]">
+                        <select class="check-restriction-toggle"
+                                data-target="<?=$item['name']?>-structure-type-toggle"
+                                name="<?=$item['name']?>-type[]">
                             <?php foreach( $possibleTypes as $possibleType => $label ): ?>
                                 <option <?=$possibleType === $item['type']? "selected": ""?>
+                                        data-restrictions="<?=in_array($possibleType, $ingredients)? "off": "on"?>"
                                         value="<?=$possibleType?>">
                                     <?=$label?>
                                 </option>
@@ -98,8 +63,15 @@ $this->addJsFile('triggers.js');
                     <li>
                         <div>Mandatory</div>
                         <input  type="checkbox" value="1" 
-                                name="content-mandatory[] ?>" 
+                                name="<?=$item['name']?>-mandatory[] ?>" 
                                 <?=$item['mandatory'] ?? null? "checked": ""?> />
+                    </li>
+                    <li <?=in_array($item['type'], $ingredients)? 'style="display: none"': ''?>
+                        id="<?=$item['name']?>-structure-type-toggle">
+                        <?php 
+                            $require    = $item['require'] ?? [];
+                            $name       = $item['name'];
+                            include $this->getIncludeDesignFile('edit/structure-require.php'); ?>
                     </li>
                 </ul>
             </fieldset>
@@ -138,12 +110,13 @@ $this->addJsFile('triggers.js');
 
 
 <style>
-    .global-data, 
+    .restriction-settings {
+        width: 100%;
+    }
     .fieldsets-container {
         max-width: 700px;
         margin-top: 10px;
     }
-    ul.global-data li,
     .fieldsets-container fieldset li {
         display: flex;
         justify-content: space-between;
@@ -189,6 +162,58 @@ $this->addJsFile('triggers.js');
 <script>
     $(document).ready(function() {
 
+        // RESTRICTIONS 
+        $('.check-restriction-toggle').change((e) => {
+            let idRestrictionDom = e.target.attributes["data-target"].value;
+            let enabled          = $(e.target).find('option:selected').data('restrictions') === "on";
+
+            if( enabled ){
+                $('#'+idRestrictionDom).show();
+            }
+            else {
+                $('#'+idRestrictionDom).hide();
+            }
+        });
+
+        $('.content-add-trigger').change((e) => {
+            let candidateItem   = e.target.value;
+            let candidateLabel  = $(e.target).find('option:selected').html().trim();
+            let actionName      = e.target.attributes["data-target"].value;
+            let items           = document.querySelectorAll('[name="'+actionName+'[]"]');
+
+            $(e.target).val( 0 );
+
+            if( Array.from(items)
+                        .map( (input) => input.value )
+                        .includes( candidateItem ) 
+            ){  return; }
+
+            let newEntry = document.createElement("a");
+            newEntry.classList.add('remove-content');
+
+            let newInput = document.createElement("input");
+            newInput.setAttribute('type', 'hidden');
+            newInput.setAttribute('name', actionName+'[]');
+            newInput.setAttribute('value', candidateItem);
+
+            newEntry.appendChild( newInput );
+
+            newEntry.appendChild( document.createTextNode(' '+candidateLabel+' ') );
+
+            let newIcon = document.createElement("i");
+            newIcon.classList.add('fa');
+            newIcon.classList.add('fa-times');
+
+            newEntry.appendChild( newIcon );
+
+            $('#'+actionName+'-contents').append( newEntry );
+        });
+
+        $('.restriction-settings').on('click', '.remove-content', function(){
+            $(this).remove();
+        });        
+
+        // NAME / FILE (hidden inputs)
         ['name', 'file'].forEach((hiddenInput) => {
             $('#'+hiddenInput+'-display').click(function(){
                 $('#'+hiddenInput+'-input').show().focus();
@@ -207,6 +232,7 @@ $this->addJsFile('triggers.js');
             });
         });
 
+        // FIELDSETS
         $("fieldset > legend > a.remove-fieldset").click(function(){
             if( confirm('Confirm Remove') ){
                 $(this).parent('legend').parent('fieldset').remove();
