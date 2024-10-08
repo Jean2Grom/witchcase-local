edbf816edf7335485d6d258aa9db82af1d62fbc1
jean2grom <jean.de.gromard@gmail.com>
2024-10-08 15:40:54 +0200

    fix form intregrated trigger buttons

--- sites/admin/assets/js/triggers.js
+++ sites/admin/assets/js/triggers.js
@@ -1,34 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     document.querySelectorAll(".trigger-href").forEach( 
-        trigger => trigger.addEventListener('click', () => {
-            if( !trigger.classList.contains("disabled") ){
-                window.location.href = trigger.dataset.href;
+        trigger => trigger.addEventListener('click', 
+            e => {
+                e.preventDefault();
+                if( !trigger.classList.contains("disabled") ){
+                    window.location.href = trigger.dataset.href;
+                }
+                return false;
             }
-            return false;
-        })    
+        )    
     );
 
     document.querySelectorAll(".trigger-action").forEach( 
-        trigger => trigger.addEventListener('click', () => {
-            if( trigger.classList.contains("disabled")
-                || trigger.dataset.action === undefined 
-                ||  trigger.dataset.target === undefined 
-                || (trigger.dataset.confirm !== undefined && !confirm( trigger.dataset.confirm ))
-            ){
+        trigger => trigger.addEventListener('click', 
+            e => {
+                e.preventDefault();
+
+                if( trigger.classList.contains("disabled")
+                    || trigger.dataset.action === undefined 
+                    ||  trigger.dataset.target === undefined 
+                    || (trigger.dataset.confirm !== undefined && !confirm( trigger.dataset.confirm ))
+                ){
+                    return false;
+                }
+
+                let action = document.createElement('input');
+                action.setAttribute('type', "hidden");
+                action.setAttribute('name', "action");
+                action.value = trigger.dataset.action;
+
+                let form = document.querySelector('#' + trigger.dataset.target);
+                form.append(action);
+                form.submit();
+
                 return false;
             }
-
-            let action = document.createElement('input');
-            action.setAttribute('type', "hidden");
-            action.setAttribute('name', "action");
-            action.value = trigger.dataset.action;
-
-            let form = document.querySelector('#' + trigger.dataset.target);
-            form.append(action);
-            form.submit();
-
-            return false;
-        })
+        )
     );
 });
