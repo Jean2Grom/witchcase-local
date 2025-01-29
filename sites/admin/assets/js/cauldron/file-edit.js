715a7857343cb6ea2c93131db1bd942ade60af9a
jean2grom <jean.de.gromard@gmail.com>
2025-01-29 17:52:20 +0100

    file edit interface update

--- sites/admin/assets/js/cauldron/file-edit.js
+++ sites/admin/assets/js/cauldron/file-edit.js
@@ -1,3 +1,100 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.querySelectorAll('.file-edit-container').forEach( container => {
+        
+        container.querySelectorAll('.switch-file-input-type a').forEach( 
+            a => a.addEventListener( 'click', () => {
+                if( a.classList.contains('selected') ){
+                    return false;
+                }
+                
+                container.querySelectorAll('.switch-file-input-type a.selected').forEach( 
+                    elmt => elmt.classList.remove('selected') 
+                );
+                
+                a.classList.add('selected');
+                
+                a.closest('.file-input').querySelectorAll('input').forEach( input => {
+                    if( input.classList.contains( a.dataset.target ) ){
+                        input.style.display = 'block';
+                    }
+                    else {
+                        input.style.display = 'none';
+                    }
+
+                    input.select();
+                    input.focus();
+                });
+            })
+        );
+        
+        container.querySelectorAll( '.file-input .upload-file-input' ).forEach(
+            input =>  input.addEventListener( 'change', () => {        
+                let filename = input.files[0].name;
+                
+                container.querySelectorAll('.file-display .new-file-focus').forEach( 
+                    span => span.innerHTML = filename
+                );
+
+                container.querySelectorAll('.file-display').forEach( 
+                    elmnt => elmnt.style.display = 'block'
+                );
+                
+                container.querySelectorAll('.file-input').forEach( 
+                    elmnt => elmnt.style.display = 'none'
+                );
+                
+                container.querySelectorAll('input.name-file-input').forEach( input => {
+                    if( input.value === '' ){
+                        input.value = filename.substring(0, filename.lastIndexOf('.'))
+                    }
+                    input.select();
+                    input.focus();
+                });
+            })
+        );
+
+        container.querySelectorAll( '.remove-file' ).forEach(
+            a =>  a.addEventListener( 'click', () => {
+                if( !confirm('Remove file ?') ){
+                    return false;
+                }
+
+                container.querySelectorAll( '.file-display .current-file-focus' ).forEach(
+                    elmnt => elmnt.style.display = 'none'
+                );
+
+                container.querySelectorAll( '.file-display' ).forEach(
+                    elmnt => elmnt.style.display = 'none'
+                );
+                
+                container.querySelectorAll( '.file-input' ).forEach(
+                    elmnt => elmnt.style.display = 'block'
+                );
+                
+                container.querySelectorAll( '.file-input .upload-file-input' ).forEach( input => {
+                    let filename    = input.files[0].name;
+                    input.value     = '';
+
+                    container.querySelectorAll('input.name-file-input').forEach( nameInput => {
+                        if( nameInput.value === filename
+                            || nameInput.value === filename.substring(0, filename.lastIndexOf('.'))  
+                        ){
+                            nameInput.value = '';
+                        }
+                        else 
+                        {
+                            nameInput.select();
+                            nameInput.focus();                
+                        }
+                    });
+                });
+            })
+        );
+
+    });
+});
+
 $(document).ready( function(){
     $('.change-file').change(function(){
         let filename = $(this).val().split(/(\\|\/)/g).pop();
--- sites/admin/design/cauldron/recipe/edit/file.php
+++ sites/admin/design/cauldron/recipe/edit/file.php
@@ -9,10 +9,108 @@ $this->wc->debug( $input );
 $key = "_FILES__".md5(microtime().rand());
 ?>
 
-<?php if( $this->exist() ): ?>
+<div class="file-edit-container">
+
+    <?php if( $this->exist() ): ?>
         <input  type="hidden" 
-                name="<?=$input?>[ID]" value="<?=$this->id ?>" />
-<?php endif; ?>
+                name="<?=$input?>[ID]" 
+                value="<?=$this->id ?>" />
+    <?php endif; ?>
+
+    <div class="file-display" <?=$path? '': 'style="display: none;"' ?>>
+        <?php if( $path ): ?>
+            <h2 class="current-file-focus">
+                Current file
+            </h2>        
+            <a  class="current-file-focus" 
+                href="/<?=$path?>" 
+                target="_blank">
+                <?=$title? $title: basename($path)?>
+            </a>
+        <?php endif; ?>
+        
+        <span class="new-file-focus"></span>
+
+        <a class="remove-file">
+            <i class="fa fa-times"></i>
+        </a>
+    </div>
+
+    <div class="file-input" <?=$path? 'style="display: none;"': '' ?>>
+        <input  type="hidden" 
+                name="<?=$input.'[content][0][name]'?>" 
+                value="path" />
+        <input  type="hidden" 
+                name="<?=$input.'[content][0][type]'?>" 
+                value="string" />
+        <input  type="hidden" 
+                name="<?=$input.'[content][0][$_FILES]'?>" 
+                value="<?=$key?>" />
+
+        <input  type="hidden" 
+                name="<?=$input.'[content][1][name]'?>" 
+                value="title" />
+        <input  type="hidden" 
+                name="<?=$input.'[content][1][type]'?>" 
+                value="string" />
+
+        <?php if( $this->content('path')?->exist() ): ?>
+            <input  type="hidden" 
+                    name="<?=$input.'[content][0][ID]'?>" 
+                    value="<?=$this->content('path')?->id ?>" />
+        <?php endif; ?>
+        <?php if( $this->content('title')?->exist() ): ?>
+            <input  type="hidden" 
+                    name="<?=$input.'[content][1][ID]'?>" 
+                    value="<?=$this->content('title')?->id ?>" />
+        <?php endif; ?>
+
+        <div class="switch-file-input-type">
+            <a  <?=$this->content('path')?->value()? '': 'class="selected"' ?> 
+                data-target="upload-file-input">
+                Upload file
+            </a>
+            /
+            <a  class="" 
+                data-target="move-file-input">
+                Move server file
+            </a>
+        </div>
+        <input  class="upload-file-input"
+                <?=$this->content('path')?->value()? 'style="display: none;"': '' ?>
+                type="file" 
+                name="<?=$key?>" />
+        <input  class="move-file-input"
+                <?=$this->content('path')?->value()? '': 'style="display: none;"' ?>
+                type="text" 
+                name="<?=$input.'[content][0][value]'?>" 
+                value="<?=$this->content('path')?->value() ?>" 
+                placeholder="enter here full path filename" />
+    </div>
+
+    <legend>Filename</legend>
+    <input  class="name-file-input"
+            type="text" 
+            name="<?=$input.'[content][1][value]'?>"
+            value="<?=$title?>" 
+            placeholder="enter filename"/>
+
+
+</div>
+
+
+<style>
+    .file-input, 
+    .file-input > .switch-file-input-type {
+        margin-bottom: 8px;
+    }
+    .file-input > .switch-file-input-type > .selected {
+        cursor: initial;
+        color: #ff9900;
+    }
+</style>
+
+<hr/>
 
 <div class="current-file-display" <?=$path? '': 'style="display: none;"' ?>>
     <?php if( $path ): ?>
